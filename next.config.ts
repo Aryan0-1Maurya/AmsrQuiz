97472ff6a9c9f828d3b2e5ab100b483fcdbcf36d
Aryan Maurya <136960772+Aryan0-1Maurya@users.noreply.github.com>
2024-12-29 06:03:15 +0000

    ready project

---
+++ next.config.ts
@@ -0,0 +1,15 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+        pathname: "**", // Matches all paths under this hostname
+      },
+    ],
+  },
+};
+
+export default nextConfig;
